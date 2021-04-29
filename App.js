e0573f4ff40ac0858ebb4f1c5e004801b22dc114
K Truong <truong.k77@gmail.com>
2021-04-29 12:05:54 -0700

    Added app loading features and ProductsModel class

--- App.js
+++ App.js
@@ -2,13 +2,14 @@ import React from "react";
 import { Alert, StatusBar } from "react-native";
 import Navigation from "./src/navigations/Navigation";
 import AppLoading from "expo-app-loading";
+import AppController from "./src/controllers/AppController";
 
 export default App = () => {
     const [isLoading, setIsLoading] = React.useState(true);
 
     async function load() {
         try {
-            // TODO: Load App Assets Here
+            await AppController.load();
             setIsLoading(false);
         } catch (error) {
             Alert.alert(ERROR, error.message, [
