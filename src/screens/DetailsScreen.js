2e1d95718cdcef1d4b2ba6032ddaf6655fa1bbd7
tuguldurbold <trae.bold@gmail.com>
2021-05-20 15:22:56 -0700

    Added padding and margin, fixed button styling and added colors to title and favorite icon.

--- src/screens/DetailsScreen.js
+++ src/screens/DetailsScreen.js
@@ -5,6 +5,7 @@ import useObservable from "../utils/useObservable";
 import AppService from "../services/AppService";
 import { Dimensions, Linking } from "react-native";
 import colours from "../../config/colours";
+import { red } from "ansi-colors";
 
 /**
  * Shows the detail of a product
@@ -56,17 +57,19 @@ export default function DetailsScreen() {
             <Image
                 style={{
                     height: Dimensions.get("window").height * 0.3,
+
                 }}
                 source={{ uri: product.image_link }}
             />
             <ListItem>
                 <ListItem.Content>
-                    <Text h4>{product.name}</Text>
+                    <Text style={{ fontWeight: '900', color: colours.primary }} h4>{product.name}</Text>
                 </ListItem.Content>
                 <Icon
                     type="material"
                     name={favourited ? "favorite" : "favorite-outline"}
                     onPress={handleFavouriteOnPress}
+                    color='red'
                 />
             </ListItem>
 
@@ -80,23 +83,24 @@ export default function DetailsScreen() {
                 <Text>Type: {product.product_type}</Text>
             </View>
 
-            <View style={{flex: 0.5}}>
-                <Text>Description:</Text>
-                <ScrollView style={{ flex: 1, backgroundColor: "white" }}>
-                    <Text>{product.description}</Text>
+            <View style={{ flex: 1, }}>
+                <Text style={{ paddingLeft: 10, fontWeight: 'bold', fontSize: 20, marginBottom: 5 }}>Description:</Text>
+                <ScrollView style={{ flex: 1, backgroundColor: "white", paddingTop: 20, paddingLeft: 10, paddingRight: 10, }}>
+                    <Text style={{ lineHeight: 22, paddingLeft: 5, paddingRight: 5, }}>{product.description}</Text>
                 </ScrollView>
             </View>
             <View style={styles.tagContainer}>
-                <Text>Tags: {product.tag_list}</Text>
+                <Text style={{ color: colours.secondary }}>Tags: {product.tag_list}</Text>
             </View>
             <Button
                 containerStyle={{
-                    margin: 16,
+                    margin: 15,
                     justifyContent: "flex-end",
-
                 }}
                 buttonStyle={{
-                    backgroundColor: colours.accent
+                    backgroundColor: colours.accent,
+                    borderRadius: 30,
+                    marginBottom: 20,
                 }}
                 title={"PURCHASE"}
                 onPress={handleBuyButton}
@@ -107,14 +111,16 @@ export default function DetailsScreen() {
 
 const styles = StyleSheet.create({
     topInfoContainer: {
-        flex: 0.2,
+        flex: 0.1,
         flexDirection: "row",
-        justifyContent: "space-evenly"
+        justifyContent: "space-evenly",
+        marginTop: 25
     },
     tagContainer: {
         flex: 0.2,
         flexDirection: "row",
         justifyContent: "space-between",
-        alignItems: "flex-end"
+        alignItems: "flex-end",
+        marginLeft: 20,
     },
 });
